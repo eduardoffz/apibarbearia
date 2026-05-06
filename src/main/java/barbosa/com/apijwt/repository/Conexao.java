203dea3876d5b2412a1489f2b4038a86b12801b6
eduardoffz <129175443+eduardoffz@users.noreply.github.com>
2026-05-06 17:18:49 -0300

    Add files via upload

---
+++ src/main/java/barbosa/com/apijwt/repository/Conexao.java
@@ -0,0 +1,28 @@
+
+package barbosa.com.apijwt.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+ public static final String url = "jdbc:mysql://localhost:3337/db_restaurante";
+    public static final String user = "root";
+    public static final String senha = "";
+    private static Connection conn = null;
+    
+    private Conexao(){
+    }
+    
+    public static synchronized Connection conectar() {
+        try {
+            if(conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(url, user, senha);
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    
+}
